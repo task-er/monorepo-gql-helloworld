c1624721b9df8c92a3afebec4231f74809783744
task-er <task.complex@gmail.com>
2022-09-09 03:11:29 +0900

    feat: add gql with apollo

--- packages/be/src/app.module.ts
+++ packages/be/src/app.module.ts
@@ -1,10 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // autoSchemaFile: true, // true로 하면 on the fly 방식. 스토리지에 저장되는듯
+      autoSchemaFile: join(process.cwd(), 'src/users/schema.gql'),
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
